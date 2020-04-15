61b39196e68041576843eb9ce66a7a778837f1cb
Arne Wendt <arne.wendt@tuhh.de>
2020-04-15 16:10:27 +0200

    working version without mail sending

--- app.js
+++ app.js
@@ -4,21 +4,62 @@ const AWS = require('aws-sdk');
 
 // helpers
 const uuid = require('uuid');
-
+const path = require('path');
+const crypto = require('crypto');
+const mime = require('mime-types');
 // web
-const app = require('express')()
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const nunjucks = require('nunjucks');
+
+const clientConfig = require('./client/client.config');
+
+
+//// user config:
+const mailSuffix = process.env.MAIL_SUFFIX;
+const secret = process.env.TOKEN_SECRET || 'MYSECRET';
+
+const appName = process.env.APP_NAME || '{tusp}';
+const publicAppBaseurl = process.env.APP_PUBLIC_BASEURL || 'http://localhost:8080/';
+const s3bucket = process.env.MINIO_DEFAULT_BUCKET;
+const s3endpoint = process.env.S3_ENDPOINT;
+const postSigner = process.env.POST_SIGN_ENDPOINT || '/postsigner';
+const port = process.env.APP_PORT || 8080;
+const useHTTPS = false;
+const cookieMaxAge = 172800000;
+const maxFileSize = process.env.MAX_FILESIZE_MB || null;
+
+//clientAssetDir relative to clientDir! (no leading or trailing slashes!)
+const clientDir = 'client';
+const clientAssetDir = 'assets';
+const renderAssetExtensions = ['.css'];
+const assetRenderData = {};
 
 
 //// setup
 // setup express
+var app = express();
 app.use(bodyParser.json());
+app.use(cookieParser())
+
+
+// setup nunjucks
+nunjucks.configure(clientDir, {
+    autoescape: true,
+    express: app
+});
+
+// client config
+var clientCfg = clientConfig.init(appName, publicAppBaseurl);
+
+
 
 // setup S3 client
 var s3  = new AWS.S3({
           accessKeyId: process.env.MINIO_ACCESS_KEY ,
           secretAccessKey: process.env.MINIO_SECRET_KEY ,
-          endpoint: 'http://localhost:' + process.env.MINIO_HOST_PORT ,
+          endpoint: s3endpoint,
           s3ForcePathStyle: true , // needed with minio?
           signatureVersion: 'v4' ,
           sslEnabled: false ,
@@ -27,42 +68,128 @@ var s3  = new AWS.S3({
 
 
 
-
 //// app
 
-// s3 presigned url & form-data generation
-app.get('/presignedUrl', (req, res) => {
-    var key = uuid.v4().replace(/-/g, '')
-    var params = {
-        Bucket: process.env.MINIO_DEFAULT_BUCKET,
-        Key: key ,
-        Fields: {
-            key: key,
-            'x-amz-meta-token': 'qwertzuiop',
-            'x-amz-meta-mail': 'user@domain.example'
-        },
-        Conditions: [
-            ["starts-with", "$x-amz-meta-filename", ""]
-        ]
-    };
+function makeToken (mail) {
+    const hmac = crypto.createHmac('sha256', secret);
+    hmac.update(mail);
+    return hmac.digest('base64');
+}
+  
+function verifyMailTokenPair (mail, token) {
+    if(!(
+      mail == undefined || mail === undefined || mail == "undefined" || mail == 'undefined' ||
+      token == undefined || token === undefined || token == "undefined" || token == 'undefined'
+      )) {
+      return token == makeToken (mail);
+    } else {
+      return false;
+    }
+}
 
-    console.log('uuid: ', key);
+function isAuth (req, cb) {
+    function parseCookies (request) {
+        var list = {},
+            rc = request.headers.cookie;
 
-    s3.createPresignedPost(params, function(err, data) {
-        if (err) {
-          console.error('Presigning post data encountered an error', err);
-        } else {
-          //console.log('The post data is', data);
-          res.end(JSON.stringify(data))
-        }
+        rc && rc.split(';').forEach(function( cookie ) {
+            var parts = cookie.split('=');
+            list[parts.shift().trim()] = decodeURIComponent(parts.join('='));
+        });
+
+        return list;
+    }
+    let cookies = parseCookies(req);
+
+    if (verifyMailTokenPair(req.query.mail, req.query.token)) {
+        console.log(req.query.mail, req.query.token);
+        cb(req.query.mail, req.query.token);
+        return true;
+    }
+    else if( verifyMailTokenPair(cookies.mail, cookies.token)) {
+        console.log(cookies.mail, cookies.token);
+        cb(cookies.mail, cookies.token);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+  
+function setCookies (res, mail, token, maxAge=cookieMaxAge) {
+    res.cookie('mail', mail, {
+      maxAge: maxAge,
+      httpOnly: true,
+      secure: useHTTPS
+     });
+    res.cookie('token', token, {
+      maxAge: maxAge,
+      httpOnly: true,
+      secure: useHTTPS
     });
+  
+}
+  
+function deleteCookies (res) {
+    setCookies(res, '', '', 0);
+}  
+
+// register mail addres // get token
+app.get('/register', function(req, res){
+    // TODO: check mail valid!?
+    
+    const mail = req.query.mailprefix + mailSuffix;
+    const token = makeToken(mail);
+
+    console.log('request for address:', mail);
+    var verificationURL=publicAppBaseurl + '?mail=' + encodeURIComponent(mail) + '&token=' + encodeURIComponent(token)
+    console.log(verificationURL)
+    
+    res.sendStatus(200);
+    
+    //not valid =>
+    //res.sendStatus(403);
+})
+
+// s3 presigned url & form-data generation
+app.get('/' + postSigner, (req, res) => {
+    if(!isAuth(req, (mail, token) => {
+        var key = uuid.v4()
+        var params = {
+            Bucket: s3bucket,
+            Key: key ,
+            Fields: {
+                key: key,
+                'x-amz-meta-token': token,
+                'x-amz-meta-mail': mail
+            },
+            Conditions: [
+                ["starts-with", "$x-amz-meta-filename", ""]
+            ]
+        };
+
+        console.log('uuid: ', key);
+
+        s3.createPresignedPost(params, function(err, data) {
+            if (err) {
+            console.error('Presigning post data encountered an error', err);
+            } else {
+            //console.log('The post data is', data);
+            res.end(JSON.stringify(data))
+            }
+        });
+    }))
+    // not authorized
+    {
+        res.sendStatus(403);
+    }
 })
 
 // s3 upload complete webhook, to keep the server application stateless
 app.post('/s3webhook', (req, res) => {
-    console.log(JSON.stringify(req.body));
+    //console.log(JSON.stringify(req.body));
     if (    req.body.EventName == 's3:ObjectCreated:Post' &&
-            req.body.Records[0].s3.bucket.name == process.env.MINIO_DEFAULT_BUCKET )
+            req.body.Records[0].s3.bucket.name == s3bucket )
     {
         var obucket = req.body.Records[0].s3.bucket.name;
         var oname = req.body.Records[0].s3.object.key;
@@ -88,13 +215,55 @@ app.post('/s3webhook', (req, res) => {
 
 // serve app; depending on auth
 app.get('/', (req, res) => {
-    // TODO
-    res.sendFile(__dirname + 'client/...');
+    // remove cookies?
+    if(req.query.deauth !== undefined){
+        //remove cookies
+        deleteCookies(res);
+        res.render('register.njk', { title: 'register | tusp', mailsuffix: mailSuffix, client: clientCfg });
+    }
+    // is authenticated? show upload form
+    else if(!isAuth(req, (mail, token) => {
+      // set cookie
+      setCookies(res, mail, token);
+      // deliver upload form
+      res.render('upload.njk', { title: 'upload | tusp', mail: mail, token: token, presigner: postSigner, maxfilesize: String(maxFileSize),
+        s3url: `${s3endpoint}/${s3bucket}`,
+        client: clientCfg.extend({
+            scripts: ["https://cdn.jsdelivr.net/npm/vue2-dropzone@3.6.0/dist/vue2Dropzone.min.js"],
+            styles: ["https://cdn.jsdelivr.net/npm/vue2-dropzone@3.6.0/dist/vue2Dropzone.min.css"]
+            })
+        });
+    }))
+    // show register form
+    {
+        res.render('register.njk', { title: 'register | tusp', mailsuffix: mailSuffix, client: clientCfg });
+    }
 })
 
-// serve stylesheets etc.
-app.use(express.static(path.join(__dirname, 'client/assets')));
+// error on unknown post
+app.post('/', (req, res) => {
+    res.sendStatus(403);
+});
+
+// serve rendered "static"
+var renderedStaticRoute = '\.*(';
+renderAssetExtensions.forEach((ext) => {
+    renderedStaticRoute += '\\' + ext + '|';
+});
+renderedStaticRoute = renderedStaticRoute.slice(0, -1) + ')';
+const assetRenderRegExp = new RegExp(renderedStaticRoute);
+app.get(assetRenderRegExp, (req, res) => {
+    assetRenderData['rand'] = uuid.v4();
+    const data = nunjucks.render(clientAssetDir + req.path, assetRenderData);
+    const type = mime.lookup(clientDir + '/'+ clientAssetDir + req.path);
+    res.set('Content-Type', type);
+    res.send(data);
+})
+
+// serve static
+app.use(express.static(path.join(__dirname, clientDir + '/' + clientAssetDir)));
 
 
 //// start app
-app.listen(process.env.APP_PORT)
+console.log('start listening on:', port);
+app.listen(port);
---
+++ client/client.config.js
@@ -0,0 +1,69 @@
+var staticConfig = {
+    local_styles: [
+        "style.css"
+    ],
+    styles: [
+        "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css",
+        "https://cdn.jsdelivr.net/npm/vue-material@1.0.0-beta-11/dist/vue-material.min.css",
+        "https://cdn.jsdelivr.net/npm/vue-material@1.0.0-beta-11/dist/theme/default.css",
+        "https://cdn.jsdelivr.net/npm/vuetify@1.5.24/dist/vuetify.min.css",
+        "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900",
+        "https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css"
+        ],
+    local_scripts: [],
+    scripts: [
+        "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
+        "https://cdn.jsdelivr.net/npm/vue-material@1.0.0-beta-11/dist/vue-material.min.js",
+        "https://cdn.jsdelivr.net/npm/vuetify@1.5.24/dist/vuetify.min.js"
+        ]
+};
+
+class clientConfig {
+    constructor(appname, baseurl) {
+        this.appname = appname;
+        this.baseurl = baseurl;
+
+        this.styles = staticConfig.styles
+        staticConfig.local_styles.forEach((style) => {
+            this.styles.push(this.baseurl + style);
+        });
+
+        this.scripts = staticConfig.scripts
+        staticConfig.local_scripts.forEach((script) => {
+            this.scripts.push(this.baseurl + script);
+        });
+    }
+
+    extend(config) {
+        var self = Object.assign({}, this);
+        if (config.styles !== undefined) {
+            self.styles = self.styles.concat(config.styles);
+        }
+        
+        if (config.scripts !== undefined) {
+            self.scripts = self.scripts.concat(config.scripts);
+        }
+
+        if (config.local_styles !== undefined) {
+                config.local_styles.forEach((style) => {
+                self.styles.push(self.baseurl + style);
+            });
+        }
+
+        if (config.local_scripts !== undefined) {
+            config.local_scripts.forEach((script) => {
+                self.scripts.push(self.baseurl + script);
+            });
+        }
+
+        return self;
+    }
+
+}
+
+function init(appname, baseurl) {
+    return new clientConfig(appname, baseurl);
+}
+
+module.exports.init = init;
+module.exports.clientConfig = clientConfig;
