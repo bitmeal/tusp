be689a2f0b4febe82e89d09ef3ce2a7c3141e05e
Arne Wendt <arne.wendt@tuhh.de>
2020-04-16 19:14:57 +0200

    basic mailing

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const nunjucks = require('nunjucks');
 
+// mailing
+const nodemailer = require("nodemailer");
+
+// frontend
 const clientConfig = require('./client/client.config');
 
 
@@ -29,9 +33,18 @@ const s3endpoint = process.env.S3_ENDPOINT;
 const postSigner = process.env.POST_SIGN_ENDPOINT || 'postsigner';
 const webhookName = process.env.WEBHOOK_NAME || 's3webhook';
 const port = process.env.APP_PORT || 8080;
-const useHTTPS = false;
+const useHTTPS = ( ( process.env.USE_HTTPS && process.env.USE_HTTPS == "true" ) ? true : false );
 const cookieMaxAge = 172800000;
 const maxFileSize = process.env.MAX_FILESIZE_MB || null;
+
+const mailing = ( ( process.env.MAILING_ENABLED && process.env.MAILING_ENABLED == "true" ) ? true : false );
+const mailingDemo = ( ( process.env.MAILING_DEMO && process.env.MAILING_DEMO == "true" ) ? true : false );
+const mailFrom = process.env.MAIL_FROM || 'tusp';
+const smtpPort = process.env.SMTP_PORT || 25;
+const smtpServer = process.env.SMTP_SERVER || 'mailer';
+const smtpUser = process.env.SMTP_USER || '';
+const smtpPass = process.env.SMTP_PASS || '';
+const smtpSecure = ( ( process.env.SMTP_SECURE && process.env.SMTP_SECURE == "true" ) ? true : false );
 
 //clientAssetDir relative to clientDir! (no leading or trailing slashes!)
 const clientDir = 'client';
@@ -53,9 +66,9 @@ nunjucks.configure(clientDir, {
     express: app
 });
 
-// client config
-var clientCfg = clientConfig.init(appName, publicAppBaseurl);
 
+// client/front-end config
+var clientCfg = clientConfig.init(appName, publicAppBaseurl);
 
 
 // setup S3 client
@@ -70,6 +83,48 @@ var s3  = new AWS.S3({
 });
 
 
+// setup mailing
+var transporter;
+if(mailing)
+{
+    if(mailingDemo)
+    {
+        nodemailer.createTestAccount((err, account) => {
+            if(err) {
+                console.error('creating mail account failed:', err);
+                process.exit(1);
+            }
+
+            console.log('created temporary account @ethereal.email');
+            console.log('user:', account.user);
+            console.log('pass:', account.pass);
+            // create reusable transporter object using the default SMTP transport
+            transporter = nodemailer.createTransport({
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: account.user, // generated ethereal user
+                    pass: account.pass // generated ethereal password
+                }
+            });
+        });
+    }
+    else
+    {
+        transporter = nodemailer.createTransport({
+            host: smtpServer,
+            port: smtpPort,
+            secure: false,
+            auth: {
+                user: smtpUser,
+                pass: smtpPass
+            }
+        });
+
+    }
+
+}
 
 //// app
 
@@ -118,7 +173,7 @@ function isAuth (req, cb) {
         return false;
     }
 }
-  
+
 function setCookies (res, mail, token, maxAge=cookieMaxAge) {
     res.cookie('mail', mail, {
       maxAge: maxAge,
@@ -135,7 +190,7 @@ function setCookies (res, mail, token, maxAge=cookieMaxAge) {
   
 function deleteCookies (res) {
     setCookies(res, '', '', 0);
-}  
+}
 
 // register mail addres // get token
 app.get('/register', function(req, res){
@@ -151,8 +206,25 @@ app.get('/register', function(req, res){
     console.log('request for address:', mail);
     var verificationURL=publicAppBaseurl + '?mail=' + encodeURIComponent(mail) + '&token=' + encodeURIComponent(token)
     console.log(verificationURL)
-        
-    res.sendStatus(200);
+    
+    let registerMail = {
+        from: mailFrom,
+        to: mail,
+        subject: `Your access-token for ${appName}`,
+        text: `Visit this link to access ${appName}:\n${verificationURL}\n\nCheers {tusp}`
+    };
+    
+    if (mailing) { 
+        transporter.sendMail(registerMail, (err, info) => {
+            if(err){
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    } else {
+        res.sendStatus(200);
+    }
 })
 
 // s3 presigned url & form-data generation
@@ -308,7 +380,7 @@ app.post('/' + webhookName, (req, res) => {
         var ofname = req.body.Records[0].s3.object.userMetadata['X-Amz-Meta-Filename'];
 
         //test token and mail, possibly delete if not valid
-        var oomail = req.body.Records[0].s3.object.userMetadata['X-Amz-Meta-Mail'];
+        var mail = req.body.Records[0].s3.object.userMetadata['X-Amz-Meta-Mail'];
 
 
         var url = s3.getSignedUrl('getObject', {
@@ -318,8 +390,17 @@ app.post('/' + webhookName, (req, res) => {
             ResponseContentDisposition: 'attachment; filename ="' + ofname + '"'
         });
 
-        console.log('Send mail to: ', oomail);
+        console.log('Send mail to: ', mail);
         console.log(url)
+
+        let downloadMail = {
+            from: mailFrom,
+            to: mail,
+            subject: `Your download for '${ofname}' from ${appName} is ready!`,
+            text: `Download '${ofname}':\n${url}\n\nCheers {tusp}`
+          };
+        
+        if (mailing) { transporter.sendMail(downloadMail); };
     }
 
     res.send("");
